7584c206269723ab626dda7df2f5a8896ea6ef83
ahamza1 <ahamza1@etf.unsa.ba>
2018-07-08 14:15:00 +0200

    Chart test

--- bss_diabetes/bss_diabetes/static/bundles/main.js
+++ bss_diabetes/bss_diabetes/static/bundles/main.js
@@ -1,1 +1,3 @@
-import "file-loader:../../../node_modules/chart.js/dist/Chart.bundle.js"
+import "file-loader:../../../node_modules/chart.js/dist/Chart.bundle.js";
+
+import {homeCharts} from "../source/js/home_chart.js";
