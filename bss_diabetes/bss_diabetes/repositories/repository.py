6b16e0c42e25f5ae3fe5bfa17da904e96d9bbdf7
ahamza1 <ahamza1@etf.unsa.ba>
2018-07-10 23:15:58 +0200

    Update

---
+++ bss_diabetes/bss_diabetes/repositories/repository.py
@@ -0,0 +1,7 @@
+from bss_diabetes.models.patient import DailySample
+from bss_diabetes.extensions import database as db
+
+class Repository:
+
+    def load_diabetes_activity_data(self):
+        database
